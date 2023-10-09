77d6f3276f221c06c5878916b2eeb59333ac5965
Song Hanyao <92096395+songxiao1018@users.noreply.github.com>
2023-10-09 13:47:20 +0800

    1009

--- scripts/1.js
+++ scripts/1.js
@@ -1,8 +1,5 @@
+function hi(){
+    console.log("hi")
+}
 
-    alert('我就是最亮的崽！')
-    document.write("Hello World!");
-
-    var x = 5;
-    var x = 6;
-    var z = x;
-    document.write(z);
+hi()
